32a3974edb45f1192d3c5e145e08caa64532b328
Emirhan Buğdaycı <148485417+Emirhanbgdyc@users.noreply.github.com>
2024-12-12 17:53:08 +0300

    Update search_screen.dart
    Duzenleme yapıldı

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -45,13 +45,13 @@ class SearchScreen extends StatelessWidget {
                    margin: EdgeInsets.only(
                      left: 25,
                    ),
-                   width: 200,  // Resmin genişliği
-                   height: 150, // Resmin yüksekliği
+                   width: 200, 
+                   height: 150,
                    decoration: BoxDecoration(
-                     borderRadius: BorderRadius.circular(20),  // Köşe yuvarlama
+                     borderRadius: BorderRadius.circular(20), 
                      image: DecorationImage(
-                       image: AssetImage('assets/image/futbolcategory.jpg'),  // Resmin yolu
-                       fit: BoxFit.cover,  // Resmin boyutunu kapsayacak şekilde yerleştirir
+                       image: AssetImage('assets/image/futbolcategory.jpg'),  
+                       fit: BoxFit.cover, 
                      ),
                    ),
                    child: Stack(
@@ -72,13 +72,13 @@ class SearchScreen extends StatelessWidget {
                    margin: EdgeInsets.only(
                      left: 25,
                    ),
-                   width: 200,  // Resmin genişliği
-                   height: 150, // Resmin yüksekliği
+                   width: 200,  
+                   height: 150, 
                    decoration: BoxDecoration(
-                     borderRadius: BorderRadius.circular(20),  // Köşe yuvarlama
+                     borderRadius: BorderRadius.circular(20),
                      image: DecorationImage(
-                       image: AssetImage('assets/image/basketbolcategory.jpg'),  // Resmin yolu
-                       fit: BoxFit.cover,  // Resmin boyutunu kapsayacak şekilde yerleştirir
+                       image: AssetImage('assets/image/basketbolcategory.jpg'), 
+                       fit: BoxFit.cover,  
                      ),
                    ),
                    child: Stack(
@@ -104,13 +104,13 @@ class SearchScreen extends StatelessWidget {
                         left: 25,
                         top: 10,
                       ),
-                      width: 200,  // Resmin genişliği
-                      height: 150, // Resmin yüksekliği
+                      width: 200,  
+                      height: 150, 
                       decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),  // Köşe yuvarlama
+                        borderRadius: BorderRadius.circular(20), 
                         image: DecorationImage(
-                          image: AssetImage('assets/image/voleybolcategory.jpg'),  // Resmin yolu
-                          fit: BoxFit.cover,  // Resmin boyutunu kapsayacak şekilde yerleştirir
+                          image: AssetImage('assets/image/voleybolcategory.jpg'), 
+                          fit: BoxFit.cover, 
                         ),
                       ),
                       child: Stack(
@@ -132,13 +132,13 @@ class SearchScreen extends StatelessWidget {
                         left: 25,
                         top: 10,
                       ),
-                      width: 200,  // Resmin genişliği
-                      height: 150, // Resmin yüksekliği
+                      width: 200, 
+                      height: 150, 
                       decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),  // Köşe yuvarlama
+                        borderRadius: BorderRadius.circular(20), 
                         image: DecorationImage(
-                          image: AssetImage('assets/image/teniscategory.jpg'),  // Resmin yolu
-                          fit: BoxFit.cover,  // Resmin boyutunu kapsayacak şekilde yerleştirir
+                          image: AssetImage('assets/image/teniscategory.jpg'),  
+                          fit: BoxFit.cover,  
                         ),
                       ),
                       child: Stack(
@@ -164,13 +164,13 @@ class SearchScreen extends StatelessWidget {
                         left: 25,
                         top: 10,
                       ),
-                      width: 200,  // Resmin genişliği
-                      height: 150, // Resmin yüksekliği
+                      width: 200, 
+                      height: 150, 
                       decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),  // Köşe yuvarlama
+                        borderRadius: BorderRadius.circular(20),
                         image: DecorationImage(
-                          image: AssetImage('assets/image/formulacategory.jpg'),  // Resmin yolu
-                          fit: BoxFit.cover,  // Resmin boyutunu kapsayacak şekilde yerleştirir
+                          image: AssetImage('assets/image/formulacategory.jpg'), 
+                          fit: BoxFit.cover, 
                         ),
                       ),
                       child: Stack(
@@ -192,13 +192,13 @@ class SearchScreen extends StatelessWidget {
                         left: 25,
                         top: 10,
                       ),
-                      width: 200,  // Resmin genişliği
-                      height: 150, // Resmin yüksekliği
+                      width: 200,  
+                      height: 150,
                       decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),  // Köşe yuvarlama
+                        borderRadius: BorderRadius.circular(20),  
                         image: DecorationImage(
-                          image: AssetImage('assets/image/naimcategory.jpg'),  // Resmin yolu
-                          fit: BoxFit.cover,  // Resmin boyutunu kapsayacak şekilde yerleştirir
+                          image: AssetImage('assets/image/naimcategory.jpg'), 
+                          fit: BoxFit.cover,  
                         ),
                       ),
                       child: Stack(
