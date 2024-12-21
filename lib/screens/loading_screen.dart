50714e4ec016f0c865dc88a72af59e62ee6bfabc
Emirhan Buğdaycı <148485417+Emirhanbgdyc@users.noreply.github.com>
2024-12-21 19:44:40 +0300

    Yüklenme sayfasında değişiklikler yapildi

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,8 +1,8 @@
-import 'package:dotlottie_loader/dotlottie_loader.dart';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:go_router/go_router.dart';
-import 'package:lottie/lottie.dart';
+import '../core/constants.dart';
 
 class LoadingScreen extends StatelessWidget {
   const LoadingScreen({super.key});
@@ -10,48 +10,87 @@ class LoadingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: SizedBox.expand(
-       // width: double.infinity,
-        child: Column(
-          children: [
-            // Logo bölümü
-            Expanded(
-              child: Container(
-                width: 250,
-                height: 250,
-                child: Image.asset(
-                  'assets/image/markaj.jpg',
-                  fit: BoxFit.contain,
+      backgroundColor: colors["white"],
+      body: Column(
+        children: [
+          Expanded(
+            child: Container(
+              width: double.infinity,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [
+                    Color.fromRGBO(146, 35, 42, 1),
+                    Color.fromRGBO(110, 25, 30, 0.8),
+                  ],
+                ),
+                borderRadius: BorderRadius.only(
+                  bottomRight: Radius.circular(32),
+                  bottomLeft: Radius.circular(32),
                 ),
               ),
-            ),
-
-
-            // Yükleniyor yazısı
-           // CircularProgressIndicator(),
-
-
-            InkWell(
-              onTap: () => context.go("/home"),
-              child: SizedBox(
-                width: 300,
-                child: DotLottieLoader.fromAsset("assets/motions/loading.lottie",
-                    frameBuilder: (BuildContext ctx, DotLottie? dotlottie) {
-                      if (dotlottie != null) {
-                        return Lottie.memory(dotlottie.animations.values.single);
-                      } else {
-                        return Container();
-                      }
-                    }),
+              child: Column(
+                children: [
+                  Expanded(
+                    flex: 1,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text("MARKAJ",style:
+                          TextStyle(
+                            color: colors["white"],
+                            fontWeight: FontWeight.bold,
+                            fontSize: 40,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    flex: 5,
+                    child: Image.asset("assets/image/futbolcategory.jpg",width: 400,),
+                  ),
+                ],
               ),
             ),
-
-            //Birazcık boşluk lazım
-            const SizedBox(height: 20,)
-
-          ],
-        ),
+          ),
+          Container(
+            width: double.infinity,
+            height: 150,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Container(
+                  padding:EdgeInsets.only(
+                  top: 14,
+                  bottom: 14,
+                  left: 12,
+                  right: 12
+                  ),
+                  margin: EdgeInsets.only(
+                    bottom: 10,
+                    right: 8,
+                    left: 8,
+                  ),
+                  decoration: BoxDecoration(
+                    color: colors["blackColor"]
+                  ),
+                  width: double.infinity,
+                  child: Center(
+                      child: InkWell(
+                        onTap: () => context.go("/home"),
+                        child: Text("GET STARTED...",
+                        style: TextStyle(
+                          color: colors["white"],
+                          fontWeight: FontWeight.bold,
+                        ),
+                        ),
+                      ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
