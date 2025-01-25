4a5afcc3e9011b41d4f7316c34b8e4916498083b
Emirhan Buğdaycı <148485417+Emirhanbgdyc@users.noreply.github.com>
2025-01-25 16:42:49 +0300

    main değişti

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:markaj_gazetesi/core/themes.dart';
 import 'core/routes.dart';
 
-
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
@@ -15,10 +14,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp.router(
       title: 'Markaj Gazetesi',
-      themeMode:ThemeMode.light,
+      themeMode: ThemeMode.light,
       theme: lightTheme,
       darkTheme: darkTheme,
-      routerConfig: router,
+      routerConfig: router, // Router burada tanımlı
       debugShowCheckedModeBanner: false,
     );
   }
