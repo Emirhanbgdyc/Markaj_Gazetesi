f1f50106f49e4b633fee01441b227a14a4f41d60
Emirhan Buğdaycı <148485417+Emirhanbgdyc@users.noreply.github.com>
2025-01-26 15:59:54 +0300

    Tenis sayfasi duzenlendi

---
+++ lib/screens/category_screens/tenis_category.dart
@@ -0,0 +1,437 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:markaj_gazetesi/screens/News_screens/News_detail_two.dart';
+import 'package:markaj_gazetesi/widgets/BottomMenu.dart';
+import 'package:markaj_gazetesi/widgets/DrawerMenu.dart';
+
+
+class TennisCategoryScreen extends StatelessWidget {
+  const TennisCategoryScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.onError,
+        title: Text(
+          "MARKAJ",
+          style: Theme.of(context).textTheme.headlineLarge,
+        ),
+        centerTitle: true,
+      ),
+      backgroundColor: Theme.of(context).colorScheme.onError,
+      body: ListView(
+        children: [
+          Column(
+            children: [
+              SizedBox(
+                  height: 60,
+                  width: MediaQuery.of(context).size.width,
+                  child:ListView(
+                    scrollDirection: Axis.horizontal,
+                    children: [
+                      Container(
+                        margin: EdgeInsets.symmetric(horizontal: 8),
+                        child: Row(
+                          children: [
+                            SizedBox(width: 12),
+                            GestureDetector(
+                              onTap: (){
+                                context.go('/home');
+                              },
+                              child: Text(
+                                "Tümü",
+                                style: Theme.of(context).textTheme.titleMedium!.copyWith(
+                                  color: Theme.of(context).colorScheme.onSecondary,
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.symmetric(horizontal: 8),
+                        child: Row(
+                          children: [
+                            SizedBox(width: 12),
+                            Text(
+                              "Gündem",
+                              style: Theme.of(context).textTheme.titleMedium!.copyWith(
+                                color: Theme.of(context).colorScheme.onSecondary,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.symmetric(horizontal: 8),
+                        child: Row(
+                          children: [
+
+                            SizedBox(width: 12),
+                            GestureDetector(
+                              onTap: (){
+                                context.go("/basketbolcategory");
+                              },
+                              child: Text(
+                                "Basketbol",
+                                style: Theme.of(context).textTheme.titleMedium!.copyWith(
+                                  color: Theme.of(context).colorScheme.onSecondary,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.symmetric(horizontal: 8),
+                        child: Row(
+                          children: [
+                            SizedBox(width: 12),
+                            GestureDetector(
+                              onTap: (){
+                                context.go("/futbolcategory");
+                              },
+                              child: Text(
+                                "Futbol",
+                                style: Theme.of(context).textTheme.titleMedium!.copyWith(
+                                  color: Theme.of(context).colorScheme.onSecondary,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.symmetric(horizontal: 8),
+                        child: Row(
+                          children: [
+                            SizedBox(width: 12),
+                            GestureDetector(
+                              onTap: (){
+                                context.go("/voleybolcategory");
+                              },
+                              child: Text(
+                                "Voleybol",
+                                style: Theme.of(context).textTheme.titleMedium!.copyWith(
+                                  color: Theme.of(context).colorScheme.onSecondary,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Center(
+                            child: Padding(
+                              padding: const EdgeInsets.only(
+                                  top: 5,
+                                  left: 10
+                              ),
+                              child: Text(
+                                "Tenis",
+                                style: Theme.of(context).textTheme.titleMedium!.copyWith(
+                                  color: Theme.of(context).colorScheme.onSecondary,
+                                ),
+                              ),
+                            ),
+                          ),
+                          SizedBox(height: 2),
+                          Padding(
+                            padding: const EdgeInsets.only(
+                                left: 10
+                            ),
+                            child: Align(
+                              alignment: Alignment.center,
+                              child: Container(
+                                height: 2,
+                                width:50,
+                                color: Theme.of(context).colorScheme.onSecondary,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  )
+
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Expanded(
+                    child: GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const NewsDetailTwoScreen(),
+                          ),
+                        );
+                      },
+                      child: Column(
+                        children: [
+                          Container(
+                            margin: EdgeInsets.all(8.0),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(16),
+                              image: DecorationImage(
+                                image: AssetImage('assets/image/newsgsone.jpg'),
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                            height: 200,
+                            width: double.infinity,
+                          ),
+                          Container(
+                            margin: EdgeInsets.only(
+                              left: 10,
+                            ),
+                            child: Text(
+                              "Peñarol, Haziran'da Fernando Muslera'yı ülkesine geri getiriyor.",
+                              style: Theme.of(context).textTheme.labelLarge!.copyWith(
+                                color: Theme.of(context).colorScheme.onSecondary,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Column(
+                      children: [
+                        GestureDetector(
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => const NewsDetailTwoScreen(),
+                              ),
+                            );
+                          },
+                          child: Container(
+                            margin: EdgeInsets.all(8.0),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(16),
+                              image: DecorationImage(
+                                image: AssetImage('assets/image/futbolcategory.jpg'),
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                            height: 160,
+                            width: double.infinity,
+                          ),
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(
+                            left: 20,
+                          ),
+                          width: 250,
+                          child: Text(
+                            "Kanat rotasyonunu güçlendirmek isteyen Trabzonspor, Espanyol forması giyen Javi Puado ile ilgileniyor.",
+                            style: Theme.of(context).textTheme.labelLarge!.copyWith(
+                              color: Theme.of(context).colorScheme.onSecondary,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Expanded(
+                    child: Column(
+                      children: [
+                        GestureDetector(
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => const NewsDetailTwoScreen(),
+                              ),
+                            );
+                          },
+                          child: Container(
+                            margin: EdgeInsets.all(8.0),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(16),
+                              image: DecorationImage(
+                                image: AssetImage('assets/image/futbolcategory.jpg'),
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                            height: 160,
+                            width: double.infinity,
+                          ),
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(
+                            left: 20,
+                          ),
+                          width: 250,
+                          child: Text(
+                            "Kanat rotasyonunu güçlendirmek isteyen Trabzonspor, Espanyol forması giyen Javi Puado ile ilgileniyor.",
+                            style: Theme.of(context).textTheme.labelLarge!.copyWith(
+                              color: Theme.of(context).colorScheme.onSecondary,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    child: GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const NewsDetailTwoScreen(),
+                          ),
+                        );
+                      },
+                      child: Column(
+                        children: [
+                          Container(
+                            margin: EdgeInsets.all(8.0),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(16),
+                              image: DecorationImage(
+                                image: AssetImage('assets/image/newsgsone.jpg'),
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                            height: 200,
+                            width: double.infinity,
+                          ),
+                          Container(
+                            margin: EdgeInsets.only(
+                              left: 10,
+                            ),
+                            child: Text(
+                              "Peñarol, Haziran'da Fernando Muslera'yı ülkesine geri getiriyor.",
+                              style: Theme.of(context).textTheme.labelLarge!.copyWith(
+                                color: Theme.of(context).colorScheme.onSecondary,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+
+                ],
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Expanded(
+                    child: GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const NewsDetailTwoScreen(),
+                          ),
+                        );
+                      },
+                      child: Column(
+                        children: [
+                          Container(
+                            margin: EdgeInsets.all(8.0),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(16),
+                              image: DecorationImage(
+                                image: AssetImage('assets/image/newsgsone.jpg'),
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                            height: 200,
+                            width: double.infinity,
+                          ),
+                          Container(
+                            margin: EdgeInsets.only(
+                              left: 10,
+                            ),
+                            child: Text(
+                              "Peñarol, Haziran'da Fernando Muslera'yı ülkesine geri getiriyor.",
+                              style: Theme.of(context).textTheme.labelLarge!.copyWith(
+                                color: Theme.of(context).colorScheme.onSecondary,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Column(
+                      children: [
+                        GestureDetector(
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => const NewsDetailTwoScreen(),
+                              ),
+                            );
+                          },
+                          child: Container(
+                            margin: EdgeInsets.all(8.0),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(16),
+                              image: DecorationImage(
+                                image: AssetImage('assets/image/futbolcategory.jpg'),
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                            height: 160,
+                            width: double.infinity,
+                          ),
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(
+                            left: 20,
+                          ),
+                          width: 250,
+                          child: Text(
+                            "Kanat rotasyonunu güçlendirmek isteyen Trabzonspor, Espanyol forması giyen Javi Puado ile ilgileniyor.",
+                            style: Theme.of(context).textTheme.labelLarge!.copyWith(
+                              color: Theme.of(context).colorScheme.onSecondary,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 30,
+              ),
+            ],
+          )
+
+
+
+        ],
+      ),
+
+      drawer:DrawerMenu(context),
+      drawerScrimColor: Colors.transparent,
+      bottomNavigationBar: const BottomMenu(),
+    );
+  }
+}
